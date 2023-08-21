c3f39c526438dc94a1b75503cdd73d8698972493
MattGunn93 <53714426+MattGunn93@users.noreply.github.com>
2023-08-21 11:46:27 -0500

    Completed logic, refactored, and added a log

--- Letter-Sorter/Letter-Sorter/Program.cs
+++ Letter-Sorter/Letter-Sorter/Program.cs
@@ -1,36 +1,43 @@
-﻿namespace Letter_Sorter
+﻿using System.Text;
+
+namespace Letter_Sorter
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			var combinedLettersPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\CombinedLetters";
+			#region Fields
+			var ls = new LetterService();
+			var log = new StringBuilder(DateTime.Now.ToString("MM/dd/yyyy") + " Report\n--------------------------------\n\nNumber of Combined Letters: ");
 			string date = DateTime.Now.ToString("yyyyMMdd");
+			// Paths & Directories
+			var combinedLettersPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\CombinedLetters";
 			var admissionDir = new DirectoryInfo(combinedLettersPath + @"\Input\Admission\" + date);
 			var scholarshipDir = new DirectoryInfo(combinedLettersPath + @"\Input\scholarship\" + date);
 			string archivePath = combinedLettersPath + @"\Archive\" + date;
 			string outputPath = combinedLettersPath + @"\Output\" + date;
+			#endregion
 
-			if (!Directory.Exists(archivePath) || !Directory.Exists(outputPath))
+			// Creating output directories
+			try
 			{
-				Directory.CreateDirectory(archivePath);
+				Directory.CreateDirectory(archivePath + @"\Admission");
+				Directory.CreateDirectory(archivePath + @"\Scholarship");
 				Directory.CreateDirectory(outputPath);
 			}
-			else
-			{
-				throw new Exception("One or both directories already exist!");
-			}
-			var archiveDir = new DirectoryInfo(archivePath);
-			var outputDir = new DirectoryInfo(outputPath);
+			catch { }
 
-			LetterService.ArchiveLetters(admissionDir, archiveDir);
-			LetterService.ArchiveLetters(scholarshipDir, archiveDir);
+			// Archiving letters
+			ls.ArchiveLetters(admissionDir, archivePath + @"\Admission");
+			ls.ArchiveLetters(scholarshipDir, archivePath + @"\Scholarship");
 
+			// Creates Dictionaries of admission and scholarship letters 
 			var admissionLetters = LetterService.GetLetters(admissionDir);
 			var scholarshipLetters = LetterService.GetLetters(scholarshipDir);
-
-			if(admissionLetters != null && scholarshipLetters != null)
+			if(admissionLetters != null)
 			{
+				List<string> combinedLetters = new List<string>();
+				int combinedCount = 0;
 				foreach (string key in admissionLetters.Keys)
 				{
 					FileInfo? admission;
@@ -41,22 +48,23 @@
 
 					if (admission != null && scholarship != null)
 					{
-						using (StreamWriter sw = File.AppendText(admission.FullName))
-						{
-							sw.WriteLine("\n");
-							using (StreamReader sr = File.OpenText(scholarship.FullName))
-							{
-								while(!sr.EndOfStream)
-								{
-									sw.WriteLine(sr.ReadLine());
-								}
-							}
-						}
+						ls.CombineTwoLetters(admission.FullName, scholarship.FullName, outputPath);
 						scholarship.Delete();
+						combinedLetters.Add(key);
+						combinedCount++;
 					}
-					admission?.MoveTo(Path.Combine(outputDir.FullName, admission.Name));
+					else
+						admission?.MoveTo(Path.Combine(outputPath, admission.Name));
+				}
+				log.Append(combinedCount.ToString()+"\n");
+				foreach(string id in combinedLetters)
+				{
+					log.AppendLine("    " + id);
 				}
 			}
+			admissionDir.Delete();
+			scholarshipDir.Delete();
+			File.AppendAllText(outputPath+@"\log.txt", log.ToString());
 		}
 	}
 	public interface ILetterService
@@ -67,7 +75,8 @@
 		/// <param name="inputFile1">File path for the first letter.</param>
 		/// <param name="inputFile2">File path for the second letter.></param>
 		/// <param name="resultFile">File path for the combined letter></param>
-		void CombineTwoLetters(string inputFile1,string inputFile2, string resultFile);
+		void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
+
 	}
 
 	public class LetterService : ILetterService
@@ -82,14 +91,26 @@
 			}
 			return value;
 		}
-		internal static void ArchiveLetters(DirectoryInfo source, DirectoryInfo target)
+		internal void ArchiveLetters(DirectoryInfo source, string target)
 		{
 			foreach (FileInfo file in source.GetFiles())
-				file.CopyTo(Path.Combine(target.FullName, file.Name));
+				file.CopyTo(Path.Combine(target, file.Name));
 		}
 		public void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile)
 		{
-
+			using (StreamWriter sw = File.AppendText(inputFile1))
+			{
+				sw.WriteLine("\n");
+				using (StreamReader sr = File.OpenText(inputFile2))
+				{
+					while (!sr.EndOfStream)
+					{
+						sw.WriteLine(sr.ReadLine());
+					}
+				}
+			}
+			var file = new FileInfo(inputFile1);
+			file?.MoveTo(Path.Combine(resultFile, file.Name));
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Letter-Sorter
 Admission and Scholarship letter sorting console app.
+
+To test the application: 
+1): Copy the included "CombinedLetters" folder in "Test Files" to the Desktop.
+2): Go to "Input\Admission" & "Input\Scholarship" folders and change the dated folder to the current date (e.g. 20230822).
+Run the application and review the copied test folder for changes.
+
+To test larger sets of files, I recommend creating a PowerShell script, or a test method which generates hundreds of admission and scholarship letters.
